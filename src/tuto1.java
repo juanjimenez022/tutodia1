c345db32083e9eb0ad640cf4030704f43cb5679b
jujis <jujis@jujise96-laptop>
2023-09-26 17:18:40 +0200

    vamos a producir collisiones

--- src/tuto1.java
+++ src/tuto1.java
@@ -4,6 +4,8 @@ public class tuto1 {
 	public static void main(String[] args) {
 		System.out.println("Hola CMEPPS");
 		System.out.println("Bienvenidos al curso");
+		System.out.println("Fallout 3 best game of the year");
+		
 
 	}
 
